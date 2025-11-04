49a792a94046bd0f1388c12e5f46ac8bbe649ef6
yogithesymbian <yogirenbox33@gmail.com>
2025-11-04 16:42:01 +0800

    refactor(viewmodel): remove unused imports
    - Remove unused `android.util.Log` and `androidx.lifecycle.SavedStateHandle` imports from `NewsHomeViewModel`.

--- app/src/main/java/com/yogiveloper/yonewsai/modules/home_news/presentation/home/NewsHomeViewModel.kt
+++ app/src/main/java/com/yogiveloper/yonewsai/modules/home_news/presentation/home/NewsHomeViewModel.kt
@@ -1,7 +1,5 @@
 package com.yogiveloper.yonewsai.modules.home_news.presentation.home
 
-import android.util.Log
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yogiveloper.yonewsai.core.util.Resource
